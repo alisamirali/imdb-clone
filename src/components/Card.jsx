358ad8f6f39fd88894b04b4b08a6d64d22f6e2fe
Ali Samir <alisamir.eng@gmail.com>
2023-05-23 13:06:39 +0300

    Edit Movie Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -22,10 +22,12 @@ export default function Card({ result }) {
           alt="image is not available"
         ></Image>
         <div className="p-2">
-          <p className="line-clamp-2 text-md">{result.overview}</p>
-          <h2 className="truncate text-lg font-bold">
+          <h3 className="truncate text-lg font-bold">
             {result.title || result.name}
-          </h2>
+          </h3>
+
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+
           <p className="flex items-center">
             {result.release_date || result.first_air_date}
             <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
